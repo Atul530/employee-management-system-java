4a852361cdee68e8cb7b28af1c747e1b8cdff1aa
Atul530 <singh.atul5914@gmail.com>
2026-09-09 20:27:12 +0530

    Adding functionallity to Add Employee Button

--- src/employee/management/system/AddEmployee.java
+++ src/employee/management/system/AddEmployee.java
@@ -2,9 +2,13 @@ package employee.management.system;
 
 import java.awt.*;
 import javax.swing.*;
-//import com.toedter.calender.JDateChooser;
+import com.toedter.calendar.JDateChooser;
+import java.util.*;
 
 public class AddEmployee extends JFrame{
+    
+    Random ran=new Random();
+    int number=ran.nextInt(999999);
     
     AddEmployee(){
         getContentPane().setBackground(Color.WHITE);
@@ -38,9 +42,98 @@ public class AddEmployee extends JFrame{
         labelDOB.setFont(new Font("serif",Font.PLAIN,20));
         add(labelDOB);
         
-//        JTextField tfname=new JTextField();
-//        tfname.setBounds(200,150,150,30);
-//        add(tfname);
+        JDateChooser dcdob=new JDateChooser();
+        dcdob.setBounds(200,200,150,30);
+        add(dcdob);
+        
+        JLabel labelsalary=new JLabel("Salary");
+        labelsalary.setBounds(400,200,150,30);
+        labelsalary.setFont(new Font("serif",Font.PLAIN,20));
+        add(labelsalary);
+        
+        JTextField tfsalary=new JTextField();
+        tfsalary.setBounds(600,200,150,30);
+        add(tfsalary);
+        
+        JLabel labeladdress=new JLabel("Address");
+        labeladdress.setBounds(50,250,150,30);
+        labeladdress.setFont(new Font("serif",Font.PLAIN,20));
+        add(labeladdress);
+        
+        JTextField tfaddress=new JTextField();
+        tfaddress.setBounds(200,250,150,30);
+        add(tfaddress);
+        
+        JLabel labelphone=new JLabel("Phone");
+        labelphone.setBounds(400,250,150,30);
+        labelphone.setFont(new Font("serif",Font.PLAIN,20));
+        add(labelphone);
+        
+        JTextField tfphone=new JTextField();
+        tfphone.setBounds(600,250,150,30);
+        add(tfphone);
+        
+        JLabel labelemail=new JLabel("Email");
+        labelemail.setBounds(50,300,150,30);
+        labelemail.setFont(new Font("serif",Font.PLAIN,20));
+        add(labelemail);
+        
+        JTextField tfemail=new JTextField();
+        tfemail.setBounds(200,300,150,30);
+        add(tfemail);
+        
+        JLabel labeleducation=new JLabel("Higher Education");
+        labeleducation.setBounds(400,300,150,30);
+        labeleducation.setFont(new Font("serif",Font.PLAIN,20));
+        add(labeleducation);
+        
+        String courses[]={"BBA","BCA","BA","B.COM","BTech","B.Sc","MBA","MCA","MA","M.COM","MTech","M.Sc","PHD"};
+        JComboBox cdeducation=new JComboBox(courses);
+        cdeducation.setBackground(Color.WHITE);
+        cdeducation.setBounds(600,300,150,30);
+        add(cdeducation);
+        
+        JLabel labeldesignation=new JLabel("Designation");
+        labeldesignation.setBounds(50,350,150,30);
+        labeldesignation.setFont(new Font("serif",Font.PLAIN,20));
+        add(labeldesignation);
+        
+        JTextField tfdesignation=new JTextField();
+        tfdesignation.setBounds(200,350,150,30);
+        add(tfdesignation);
+        
+        JLabel labelaadhar=new JLabel("Aadhar No");
+        labelaadhar.setBounds(400,350,150,30);
+        labelaadhar.setFont(new Font("serif",Font.PLAIN,20));
+        add(labelaadhar);
+        
+        JTextField tfaadhar=new JTextField();
+        tfaadhar.setBounds(600,350,150,30);
+        add(tfaadhar);
+        
+        JLabel labelempId=new JLabel("Employee ID");
+        labelempId.setBounds(50,400,150,30);
+        labelempId.setFont(new Font("serif",Font.PLAIN,20));
+        add(labelempId);
+        
+        JLabel lblempId=new JLabel(""+number);
+        lblempId.setBounds(200,400,150,30);
+        lblempId.setFont(new Font("serif",Font.PLAIN,20));
+        add(lblempId);
+        
+        JButton add = new JButton("Add Details");
+        add.setBounds(250, 550, 150, 40);
+//        add.addActionListener(this);
+        add.setBackground(Color.BLACK);
+        add.setForeground(Color.WHITE);
+        add(add);
+        
+        JButton back = new JButton("Go Back");
+        back.setBounds(450, 550, 150, 40);
+//        add.addActionListener(this);
+        back.setBackground(Color.BLACK);
+        back.setForeground(Color.WHITE);
+        add(back);
         
         setSize(900,700);
         setLocation(300,50);
